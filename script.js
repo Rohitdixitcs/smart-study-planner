3c07d23e647fd894acf404f1ac6b31042187f66c
rohit dixit <iamrohitdixit17@gmail.com>
2026-05-31 21:50:01 +0530

    Created modern dashboard layout

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Smart Study Planner Loaded");
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+let tasks = [];
+
+addTaskBtn.addEventListener("click", () => {
+
+    const taskText = taskInput.value.trim();
+
+    if(taskText === "") return;
+
+    tasks.push(taskText);
+
+    renderTasks();
+
+    taskInput.value = "";
+});
+
+function renderTasks(){
+
+    taskList.innerHTML = "";
+
+    tasks.forEach(task => {
+
+        const li = document.createElement("li");
+
+        li.textContent = task;
+
+        taskList.appendChild(li);
+
+    });
+
+    document.getElementById("totalTasks").textContent = tasks.length;
+}
